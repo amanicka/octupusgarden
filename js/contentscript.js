63b160b4664069758bea970520feb2afcd432842
Huang Li <hul019@ucsd.edu>
2014-11-09 05:18:30 -0800

    clean up

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,7 +1,4 @@
 // add bubble to the top of the page
-//var bubbleDOM = document.createElement('div');
-//bubbleDOM.setAttribute('class', 'selection_bubble');
-//document.body.appendChild(bubbleDOM);
 var speechBubble = document.createElement('div');
 speechBubble.setAttribute('class', 'triangle-isosceles');
 document.body.appendChild(speechBubble);
@@ -42,23 +39,17 @@ document.addEventListener('mouseup', function (e) {
 
 // close the bubble when we click on the screen
 document.addEventListener('mousedown', function (e) {
-  //bubbleDOM.style.visibility = 'hidden';
-    speechBubble.style.visibility = 'hidden';
+  speechBubble.style.visibility = 'hidden';
 }, false);
 
 // move that bubble to the appropriate location
 function renderBubble(mouseX, mouseY, selection) {
-  //bubbleDOM.innerHTML = selection;
-  //bubbleDOM.style.top = mouseY + 'px';
-  //bubbleDOM.style.left = mouseX + 'px';
-  //bubbleDOM.style.visibility = 'visible';
-
   speechBubble.innerHTML = selection;
-  /*var bubbleHeight = $('#speechBubble').height();
-  speechBubble.style.top = (mouseY - bubbleHeight) + 'px';*/
-  speechBubble.style.top = mouseY  + 'px';
-  speechBubble.style.left = mouseX + 'px';
+  var bubbleHeight = $('#speechBubble').height();
+  speechBubble.style.top = (mouseY - bubbleHeight - 100) + 'px';
+  /*speechBubble.style.top = mouseY  + 'px';*/
+  speechBubble.style.left = mouseX - 100 + 'px';
   speechBubble.style.position = 'fixed';
   alert("mouseX:" + mouseX + " mouseY: " + mouseY);
-  speechBubble .style.visibility = 'visible';
+  speechBubble.style.visibility = 'visible';
 }
