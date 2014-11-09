a226bdb1191f5909ba82d5c43528b70bd58604de
Huang Li <hul019@ucsd.edu>
2014-11-08 21:28:19 -0800

    added table

--- js/contentscript.js
+++ js/contentscript.js
@@ -6,12 +6,38 @@ var speechBubble = document.createElement('div');
 speechBubble.setAttribute('class', 'triangle-isosceles');
 document.body.appendChild(speechBubble);
 
+function createTable() {
+  var body = document.body,
+      tbl = document.createElement('table');
+  tbl.style.width = '350px';
+  tbl.style.height = '200px';
+  tbl.style.border = "1px solid black";
+
+  for(var i = 0; i < 4; i++) {
+    var tr = tbl.insertRow();
+    for(var j = 0; j < 6; j++) {
+      if(i == 3 && j == 6) {
+        break;
+      } else {
+        var td = tr.insertCell();
+        td.appendChild(document.createTextNode('Cell'));
+        td.style.border = '1px solid black';
+        if(i == 1 && j == 6) {
+          td.setAttribute('rowSpan', '6');
+        }
+      }
+    }
+  }
+  speechBubble.appendChild(tbl);
+}
+
 // listen to mouseup DOM events
 document.addEventListener('mouseup', function (e) {
   var selection = window.getSelection().toString();
   if (selection.length > 0) {
     renderBubble(e.clientX, e.clientY, selection);
   }
+  createTable();
 }, false);
 
 // close the bubble when we click on the screen
